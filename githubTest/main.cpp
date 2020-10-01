be6b86e274f5f1274fb99154030471ba78e8d8a2
edmondywen <edmondywen@gmail.com>
2020-09-30 21:27:46 -0700

    Initial Commit

---
+++ githubTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  githubTest
+//
+//  Created by Edmond Wen on 9/30/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
